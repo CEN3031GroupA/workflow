e8db6d4cc8d19ab0862b19ab647f17ce2aedb51d
gtbrewster <gtbrewster@ufl.edu>
2017-01-24 01:25:54 -0500

    added list total, increment in add, and decrement in delete

--- todo-src/script.js
+++ todo-src/script.js
@@ -5,22 +5,24 @@ var myApp = angular.module('app', []);
 myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
   $scope.newItem = "";
-  
+  $scope.totalItems = 2;
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
       $scope.todos.push($scope.newItem);
       $scope.newItem = "";
+      $scope.totalItems = $scope.totalItems + 1;
     }
   }
-    
+
   $scope.deleteItem = function(item){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
+    $scope.totalItems -= 1;
   }
-    
-  
+
+
 });
 
 /*************************
@@ -32,5 +34,5 @@ myApp.controller('MainCtrl', function ($scope){
  * - make it prettier
  * - add a due date
  * - add reminder (setInterval)
- * 
+ *
  * *********************/
